da086dc9c23b14c33ede7eb3718c0b5b913bb6ef
Daniel Farias <danielmfariasdev@gmail.com>
2023-11-15 13:05:49 -0300

    add new rules eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,12 +18,26 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   plugins: ['react', 'react-hooks', '@typescript-eslint'],
-  rules: {
-    "react/react-in-jsx-scope": "off",
-  },
   settings: {
     react: {
       version: 'detect',
     },
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/unbound-method': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
   },
 };
