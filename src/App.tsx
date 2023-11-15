af995d8529af028bf5ef6ccc490424edebcd25b2
Daniel Farias <danielmfariasdev@gmail.com>
2023-11-15 15:36:17 -0300

    cleanning, libs and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../public/vite.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes } from './routes';
+import { GlobalStyle } from './styles/global';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> test
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <Router>
+      <GlobalStyle />
+      <Routes />
+    </Router>
   );
 }
-
-export default App;
---
+++ src/routes.tsx
@@ -0,0 +1,11 @@
+import { Routes as RoutesContainer, Route, Navigate } from "react-router-dom";
+import { HomePage } from "./pages/Home";
+
+export function Routes() {
+  return (
+    <RoutesContainer>
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </RoutesContainer>
+  );
+}
