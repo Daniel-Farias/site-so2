8b986f86e6c4960f76ea07c0e8a401b939df6746
luizmineiro <jluizmineiro@gmail.com>
2023-11-24 18:25:18 -0300

    anchor

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -5,12 +5,20 @@ import * as S from './styles';
 export function NavBar() {
   const [extendNavbar, setExtendNavbar] = useState(false);
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+    setExtendNavbar(false); 
+  };
+
   return (
     <S.Header>
       <S.NavbarContainer open={extendNavbar}>
         <S.Img src={logo} alt="UESPI" />
         <S.NavbarLinkContainer>
-          <S.NavbarLink to="/">Home</S.NavbarLink>
+          <S.NavbarLink to="/" onClick={scrollToTop}>Home</S.NavbarLink>
           <S.NavbarLink to="/about">Memória Virtual</S.NavbarLink>
           <S.NavbarLink to="/contact">Gerência de Memória</S.NavbarLink>
           <S.Anchor href="#help">Ajuda</S.Anchor>
