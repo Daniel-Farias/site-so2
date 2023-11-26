e9918513ab7b58f59f88f156829b2e6b6fc1068d
Tiago Silva <87677785+TSCTiago@users.noreply.github.com>
2023-11-25 21:43:28 -0300

    add info cards

---
+++ src/data/posts.ts
@@ -0,0 +1,100 @@
+import GM1 from '../assets/images/cards/gerencia-de-memoria/card_introducao.png';
+import GM2 from '../assets/images/cards/gerencia-de-memoria/card_alocacao_continua_simples.png';
+import GM3 from '../assets/images/cards/gerencia-de-memoria/card_overlay.png';
+import GM4 from '../assets/images/cards/gerencia-de-memoria/card_alocacao_particionada_fixa.png';
+import GM5 from '../assets/images/cards/gerencia-de-memoria/card_alocacao_particionada_estatica_absoluta.png';
+import GM6 from '../assets/images/cards/gerencia-de-memoria/card_alocacao_particionada_dinamica.png';
+import GM7 from '../assets/images/cards/gerencia-de-memoria/card_estrategias_de_alocacao_de_particao.png';
+import GM8 from '../assets/images/cards/gerencia-de-memoria/card_swapping.png';
+
+import MV1 from '../assets/images/cards/memoria-virtual/card_introducao.png';
+import MV2 from '../assets/images/cards/memoria-virtual/card_memoria_virtual_por_paginacao.png';
+import MV3 from '../assets/images/cards/memoria-virtual/card_memoria_virtual_por_segmentacao.png';
+import MV4 from '../assets/images/cards/memoria-virtual/card_memoria_virtual_por_segmentacao_com_paginacao.png';
+import MV5 from '../assets/images/cards/memoria-virtual/card_politicas_de_substituicao_de_paginas.png';
+import MV6 from '../assets/images/cards/memoria-virtual/card_protecao_de_memoria.png';
+import MV7 from '../assets/images/cards/memoria-virtual/card_swapping.png';
+import MV8 from '../assets/images/cards/memoria-virtual/card_thrashing.png';
+
+export const posts = [
+  {
+    title: 'Introdução',
+    image: GM1,
+    categories: ['Gerência de Memória'],
+  },
+  {
+    title: 'Alocação Contígua Simples',
+    image: GM2,
+    categories: ['Gerência de Memória'],
+  },
+  {
+    title: 'Overlay',
+    image: GM3,
+    categories: ['Gerência de Memória'],
+  },
+  {
+    title: 'Alocação Particionada Estática ou Fixa',
+    image: GM4,
+    categories: ['Gerência de Memória'],
+  },
+  {
+    title: 'Alocação Particionada Estática Absoluta',
+    image: GM5,
+    categories: ['Gerência de Memória'],
+  },
+  {
+    title: 'Alocação Particionada Dinâmica',
+    image: GM6,
+    categories: ['Gerência de Memória'],
+  },
+  {
+    title: 'Estratégias de Alocação de Partição',
+    image: GM7,
+    categories: ['Gerência de Memória'],
+  },
+  {
+    title: 'Swapping',
+    image: GM8,
+    categories: ['Gerência de Memória'],
+  },
+  {
+    title: 'Introdução',
+    image: MV1,
+    categories: ['Memória Virtual'],
+  },
+  {
+    title: 'Memória Virtual por Paginação',
+    image: MV2,
+    categories: ['Memória Virtual'],
+  },
+  {
+    title: 'Memória Virtual por Segmentação',
+    image: MV3,
+    categories: ['Memória Virtual'],
+  },
+  {
+    title: 'Memória Virtual por Segmentação com Paginação',
+    image: MV4,
+    categories: ['Memória Virtual'],
+  },
+  {
+    title: 'Políticas de Substituição de Páginas',
+    image: MV5,
+    categories: ['Memória Virtual'],
+  },
+  {
+    title: 'Proteção de Memória',
+    image: MV6,
+    categories: ['Memória Virtual'],
+  },
+  {
+    title: 'Swapping em Memória Virtual',
+    image: MV7,
+    categories: ['Memória Virtual'],
+  },
+  {
+    title: 'Thrashing',
+    image: MV8,
+    categories: ['Memória Virtual'],
+  },
+];
